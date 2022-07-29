78cd49edcdd26fe6f84f81010c032424023621bb
Amer <amer1988@gmail.com>
2022-07-29 23:51:27 +0300

    added KnowBe4 Reports

---
+++ assets/js_API/phishing.js
@@ -0,0 +1,67 @@
+var myData = []
+
+function myFunction() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let response = this.responseText;
+			var json = JSON.parse(response)
+			
+			for(var i = json.length - 1; i >= 0; i--) {
+				let obj = json[i];
+				let group = obj["groups"]
+				
+				if(obj.name != "Informative" && obj.name != "Microsoft 365 Clone" && group[0]["name"] != "Only Me" && obj.delivered_count != 1) {
+					myData.push({"Name": obj.name, "Group": group[0]["name"], "Date": obj.started_at, "Delivered": obj.delivered_count,"Opened": obj.opened_count, "Clicked": obj.clicked_count, "Replied": obj.replied_count, "Attachment Open": obj.attachment_open_count, "Entered Data": obj.data_entered_count})
+				}
+				
+			}
+			document.body.appendChild(buildHtmlTable(myData))
+		}
+	};
+	xhttp.open("GET", "https://eu.api.knowbe4.com/v1/phishing/security_tests", true);
+	xhttp.setRequestHeader('Authorization', "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhaWQiOjI2MDAyLCJpYXQiOjE1ODYzNTEzMjJ9.4Fv2q4Vthl_FJawGN6CAsy8se9P5aHnUcWBCuNnNtqs");
+	xhttp.send();
+}
+
+var _table_ = document.createElement('table'),
+	_tr_ = document.createElement('tr'),
+	_th_ = document.createElement('th'),
+	_td_ = document.createElement('td');
+
+// Builds the HTML Table out of myList json data from Ivy restful service.
+function buildHtmlTable(arr) {
+	var table = _table_.cloneNode(false),
+		columns = addAllColumnHeaders(arr, table);
+	for (var i = 0, maxi = arr.length; i < maxi; ++i) {
+		var tr = _tr_.cloneNode(false);
+		for (var j = 0, maxj = columns.length; j < maxj; ++j) {
+			var td = _td_.cloneNode(false);
+			var cellValue = arr[i][columns[j]];
+			td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
+	return table;
+}
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records
+function addAllColumnHeaders(arr, table) {
+	var columnSet = [],
+		tr = _tr_.cloneNode(false);
+	for (var i = 0, l = arr.length; i < l; i++) {
+		for (var key in arr[i]) {
+			if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key) === -1) {
+				columnSet.push(key);
+				var th = _th_.cloneNode(false);
+				th.appendChild(document.createTextNode(key));
+				tr.appendChild(th);
+			}
+		}
+	}
+	table.appendChild(tr);
+	return columnSet;
+}
