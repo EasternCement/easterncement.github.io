e8f62d11352012af183d60e20eea0c77785d8376
Amer <amer1988@gmail.com>
2022-07-31 09:40:57 +0300

    changed token method

--- assets/js_API/training.js
+++ assets/js_API/training.js
@@ -1,5 +1,16 @@
 var myData = []
-var token = ""
+var token = GetURLParameter('tok');
+
+function GetURLParameter(sParam) {
+	var sPageURL = window.location.search.substring(1)
+	var sURLVariables = sPageURL.split('&')
+	for (var i = 0; i < sURLVariables.length; i++) {
+		var sParameterName = sURLVariables[i].split('=')
+		if (sParameterName[0] == sParam) {
+			return sParameterName[1]
+		}
+	}
+}
 
 function myFunction() {
 	var xhttp = new XMLHttpRequest();
